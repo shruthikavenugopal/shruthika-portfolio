3d3d587f7c3f1bbfa09a511e778c48e086090711
shruthikavenugopal <shruthikavenugopal1630@gmail.com>
2025-05-12 10:13:11 +0530

    dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+const viteLogo = `${import.meta.env.BASE_URL}vite.svg`; 
 import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
